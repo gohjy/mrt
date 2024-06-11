bb9848be7097146cb5ae2712dda7e1a2d078421c
gohjy <154421097+gohjy@users.noreply.github.com>
2024-06-11 07:13:22 +0000

    Initial commit

---
+++ basic.js
@@ -0,0 +1,426 @@
+const input = document.querySelector("#stncode");
+const btn = document.querySelector("#go");
+
+class ValueError extends Error { get name() { return "ValueError"; } };
+
+const lines = {
+	standardFormatOrder: [
+		"ns", // North-South Line
+		"ew", // East-West Line
+		"cg", // Changi Airport branch line, will be deprecated when TELe (and CRLe) opens
+		"ne", // North-East Line
+		"cc", // Circle Line
+		"ce", // Circle Line Extension, will be deprecated when CCL6 opens
+		"dt", // Downtown Line
+  	"de", // Downtown Line 2 Extension, will be supported when DTL2e opens with NS6
+		"te", // Thomson-East Coast Line
+		"js", // Jurong Region Line (South Branch)
+		"jw", // Jurong Region Line (West Branch)
+		"je", // Jurong Region Line (East Branch)
+		"cr", // Cross Island Line
+		"cp"  // Cross Island Line Punggol Extension
+	]
+};
+
+
+const stations = [
+	/*
+ 		Format:
+	[
+ 		[
+	 		["xx1", "stage3"],
+			["yy1", "stage4"]
+		],
+		"Capitalized Name"
+	]
+ ...
+ 	notes:
+	one-north station is all lowercase
+	hyphens !== spaces
+ */
+
+	// East-West Line
+	[
+		[
+			["EW1", "EWL2a2"],
+			["CR5", "CRL1"],
+			["CP1", "CPe"]
+		],
+		"Pasir Ris"
+	],
+	[
+		[
+			["EW2", "EWL2a2"],
+			["DT32", "DTL3"]
+		],
+		"Tampines"
+	],
+	[
+		[
+			["EW3", "EWL2a2"]
+		],
+		"Simei"
+	],
+	[
+		[
+			["EW4", "EWL2a1"],
+			["CG", "EWL Changi Airport Extension", "TE35", "TELe"]
+		],
+		"Tanah Merah"
+	],
+	[
+		[
+			["CG1", "EWL Changi Airport Extension", "TE34", "TELe"],
+			["DT35", "DTL3"]
+		],
+		"Expo"
+	],
+	[
+		[
+			["CG2", "EWL Changi Airport Extension", "TE33", "TELe"]
+		],
+		"Changi Airport"
+	],
+	[
+		[
+			["EW5", "EWL2a1"]
+		],
+		"Bedok"
+	],
+	[
+		[
+			["EW6", "EWL2a1"]
+		],
+		"Kembangan"
+	],
+	[
+		[
+			["EW7", "EWL2a1"]
+		],
+		"Eunos"
+	],
+	[
+		[
+			["EW8", "EWL2a1"],
+			["CC9", "CCL2"]
+		],
+		"Paya Lebar"
+	],
+	[
+		[
+			["EW9", "EWL2a1"]
+		],
+		"Aljunied"
+	],
+	[
+		[
+			["EW10", "EWL2a1"]
+		],
+		"Kallang"
+	],
+	[
+		[
+			["EW11", "EWL2a1"]
+		],
+		"Lavender"
+	],
+	[
+		[
+			["EW12", "EWL2a1"],
+			["DT14", "DTL1"]
+		],
+		"Bugis"
+	],
+
+	[
+		[
+			["NS24", "NSL2a"],
+			["NE6", "NEL"],
+			["CC1", "CCL1"]
+		],
+		"Dhoby Ghaut"
+	]
+	,
+	// Circle Line
+	[
+		[
+			["CE1", "CCLe", "CC34", "CCL6"],
+			["DT14", "DTL1"]
+		],
+		"Bayfront"
+	],
+
+	// Thomson-East Coast Line
+	[
+		[
+			["TE1", "TEL1"],
+			["RTS", "RTS Link"]
+		],
+		"Woodlands North"
+	],
+	[
+		[
+			["TE2", "TEL1"],
+			["NS9", "NSL Woodlands Extension"]
+		],
+		"Woodlands"
+	],
+	[
+		[
+			["TE3", "TEL1"]
+		],
+		"Woodlands South"
+	],
+	[
+		[
+			["TE4", "TEL2"]
+		],
+		"Springleaf"
+	],
+	[
+		[
+			["TE4A", "TEL Tagore Station"]
+		],
+		"Tagore"
+	],
+	[
+		[
+			["TE5", "TEL2"]
+		],
+		"Mayflower"
+	],
+	[
+		[
+			["TE6", "TEL2"]
+		],
+		"Springleaf"
+	],
+	[
+		[
+			["TE7", "TEL2"],
+			["CR13", "CRL1"]
+		],
+		"Bright Hill"
+	],
+	[
+		[
+			["TE8", "TEL2"]
+		],
+		"Upper Thomson"
+	],
+	[
+		[
+			["TE9", "TEL2"],
+			["CC17", "CCL4"]
+		],
+		"Caldecott"
+	],
+	[
+		[
+			["TE10", "TEL3"]
+		],
+		"Mount Pleasant"
+	],
+	[
+		[
+			["NS27", "NSL2a"],
+			["TE20", "TEL3"],
+			["CE2", "CCLe", "CC33", "CCL6"]
+		],
+		"Marina Bay"
+	],
+	[
+		[
+			["TE21", "TEL3"]
+		],
+		"Marina South"
+	],
+	[
+		[
+			["TE22", "TEL3"]
+		],
+		"Gardens by the Bay"
+	],
+	[
+		[
+			["TE22A", "TEL4"]
+		],
+		"Founders' Memorial"
+	],
+	[
+		[
+			["TE23", "TEL4"]
+		],
+		"Tanjong Rhu"
+	],
+	[
+		[
+			["TE24", "TEL4"]
+		],
+		"Katong Park"
+	],
+	[
+		[
+			["TE25", "TEL4"]
+		],
+		"Tanjong Katong"
+	],
+	[
+		[
+			["TE26", "TEL4"]
+		],
+		"Marine Parade"
+	],
+	[
+		[
+			["TE27", "TEL4"]
+		],
+		"Marine Terrace"
+	],
+	[
+		[
+			["TE28", "TEL4"]
+		],
+		"Siglap"
+	],
+	[
+		[
+			["TE29", "TEL4"]
+		],
+		"Bayshore"
+	],
+	[
+		[
+			["TE30", "TEL5"]
+		],
+		"Bedok South"
+	],
+	[
+		[
+			["TE31", "TEL5"],
+			["DT37", "DTL3e"]
+		],
+		"Sungei Bedok"
+	],
+];
+
+const stages = {
+	// East-West Line
+	"EWL1": {open: true, date: "12 Dec 1987", except: {}},
+	"EWL1a": {open: true, date: "12 Mar 1988", except: {}},
+	"EWL2a1": {open: true, date: "4 Nov 1989", except: {}},
+	"EWL2a2": {open: true, date: "16 Dec 1989", except: {}},
+	"EWL2b1": {open: true, date: "5 Nov 1988", except: {}},
+	"EWL2b2": {open: true, date: "6 Jul 1990", except: {}},
+	"EWL Changi Airport Extension": {open: true, date: "10 Jan 2001", except: {
+		"CG2": {open: true, date: "8 Feb 2002"}
+	}},
+	"EWL Dover Infill Station": {open: true, date: "18 Oct 2001", except: {}},
+	"EWL Boon Lay Extension": {open: true, date: "28 Feb 2009", except: {}},
+	"EWL Tuas West Extension": {open: true, date: "18 Jun 2017", except: {}},
+
+	// North-South Line
+	"NSL1-1": {open: true, date: "7 Nov 1987", except: {}},
+	"NSL1-2": {open: true, date: "12 Dec 1987", except: {}},
+	"NSL2a": {open: true, date: "4 Nov 1989", except: {}},
+	"NSL2b1": {open: true, date: "20 Dec 1988", except: {}},
+	"NSL2b2": {open: true, date: "10 Mar 1990", except: {}},
+	"NSL Woodlands Extension": {open: true, date: "10 Feb 1996", except: {}},
+	"NSL Marina South Extension": {open: true, date: "23 Nov 2014", except: {}},
+	"NSL Canberra Infill Station": {open: true, date: "2 Nov 2019", except: {}},
+	"NSL Brickland Infill Station": {open: false, date: "2034", except: {}},
+	"NSL Sungei Kadut Infill Station": {open: false, date: "mid-2030s", except: {}},
+
+	// North-East Line
+	"NEL": {open: true, date: "20 Jun 2003", except: {
+		"NE11": {open: true, date: "20 Jun 2011", except: {}},
+		"NE15": {open: true, date: "15 Jan 2006", except: {}}
+	}},
+	"NELe": {open: false, date: "2024", except: {}},
+
+	// Circle Line
+	"CCL1": {open: true, date: "17 Apr 2010", except: {}},
+	"CCL2": {open: true, date: "17 Apr 2010", except: {}},
+	"CCL3": {open: true, date: "28 May 2009", except: {}},
+	"CCL4": {open: true, date: "8 Oct 2011", except: {}},
+	"CCL5": {open: true, date: "8 Oct 2011", except: {}},
+	"CCLe": {open: true, date: "14 Jan 2012", except: {}},
+	"CCL6": {open: false, date: "2026", except: {}},
+	"CCL Bukit Brown Shell Station": {open: false, date: "TBA", except: {}},
+
+	// Downtown Line
+	"DTL1": {open: true, date: "22 Dec 2013", except: {}},
+	"DTL2": {open: true, date: "27 Dec 2015", except: {}},
+	"DTL2e": {open: false, date: "mid-2030s", except: {}},
+	"DTL3": {open: true, date: "21 Oct 2017", except: {}},
+	"DTL3e": {open: false, date: "2026", except: {}},
+	"DTL Hume Station": {open: false, date: "2025", except: {}},
+
+	// Thomson-East Coast Line
+	"TEL1": {open: true, date: "31 Jan 2020", except: {}},
+	"TEL2": {open: true, date: "28 Aug 2021", except: {}},
+	"TEL3": {open: true, date: "13 Nov 2022", except: {
+		"TE10": {open: false, date: "TBA", except: {}},
+		"TE21": {open: false, date: "TBA", except: {}}
+	}},
+	"TEL4": {open: false, date: "23 Jun 2024", except: {
+		"TE22A": {open: false, date: "2028", except: {}}
+	}},
+	"TEL5": {open: false, date: "2026", except: {}},
+	"TELe": {open: false, date: "by 2040", except: {}},
+	"TEL Tagore Station": {open: false, date: "TBA", except: {}},
+
+	// Jurong Region Line
+	"JRL1": {open: true, date: "2027", except: {}},
+	"JRL2": {open: true, date: "2028", except: {}},
+	"JRL3": {open: true, date: "2029", except: {}},
+	
+	// Cross Island Line
+	"CRL1": {open: false, date: "2030", except: {}},
+	"CPe": {open: false, date: "2032", except: {}},
+	"CRL2": {open: false, date: "2032", except: {}},
+	"CRL3": {open: false, date: "TBA", except: {}},
+
+	// RTS Link
+	"RTS Link": {open: false, date: "2026", except: {}}
+}
+
+function getStation(stCode) {
+	let station = stations.find(i =>  i[0].some(j => j[0].toLowerCase() === stCode.toLowerCase())) ?? null; 
+	return station;
+}
+
+function getStationName(stName) {
+
+	let station = stations.find(i =>  i[1].toLowerCase() === stName.toLowerCase()) ?? null;
+	return station;
+}
+
+
+function format(input) {
+	if (typeof input !== "string") {
+		throw new TypeError("Input must be a string");
+	}
+	input = input.trim().toLowerCase();
+	if (input === "rts") return input;
+	if 		(!lines.standardFormatOrder.includes(input.slice(0, 2))) throw new ValueError("Bad input");
+	if (!input.slice(2).match(/^([1-9]\d*[a-z]?)?$/)) throw new ValueError("Bad input");
+	return input;
+}
+
+function formatname(input) {
+	if (typeof input !== "string") {
+		throw new TypeError("Input must be a string");
+	}
+	input = input.trim().toLowerCase();
+	return input
+}
+
+function toLTG(stnName) {
+	return `https://landtransportguru.net/${
+		stnName
+			.toLowerCase()
+			.split(" ")
+			.join("-")
+	}-station/`;
+}
---
+++ user.js
@@ -0,0 +1,260 @@
+// TESTING PHASE 1
+{
+	const stnCodeSearch = function() {
+		const output = document.querySelector("#stn_output");
+		output.innerHTML = "";
+		let codeInput = document.querySelector("#stncode").value;
+		let formatted;
+		try {
+			formatted = format(codeInput);
+		} catch(e) {
+			if (e.message === "Bad input") {
+				output.innerHTML = "<em>Bad input</em>";
+				return;
+			}
+		}
+		let station = getStation(formatted);
+		if (!station) {
+			output.innerHTML = "<em>Station not found</em>";
+			return;
+		}
+		let stationName = station[1];
+		let stnCode = document.createElement("div");
+		stnCode.className = "stn-code-holder";
+		let codes = [];
+		for ([i] of station[0]) codes.push(i);
+		let newcodes = [];
+		for (arr of station[0]) newcodes.push(arr[2] ? arr[2] : arr[0]);
+		let futureChange = codes.join() !== newcodes.join();
+		for ([i] of station[0]) {
+			i = i.toLowerCase();
+			let code = document.createElement("div");
+			code.className = "stn-code " + ((i === "rts") ? "rts" :  i.slice(0, 2).toLowerCase());
+			code.append(
+				(i === "rts") ? "RTS" : 
+				i.toUpperCase().slice(0, 2) + " " + i.slice(2).toUpperCase()
+			);
+			stnCode.append(code);
+		}
+		output.append(stnCode);
+		output.append(" ");
+		output.append(stationName);
+		output.append(document.createElement("br"));
+		const partOpen = function([code, stage]) {
+			let majorOpen = stages[stage].open;
+			let minorOpen = (stages[stage].except[code]?.open) ?? majorOpen;
+			if (!majorOpen && !minorOpen) return false;
+			if (majorOpen && minorOpen) return true;
+			if (minorOpen) return true;
+			return false;
+		}
+		const getOpeningDate = function([code, stage]) {
+			let majorDate = stages[stage].date;
+			let isMinor = code in stages[stage].except;
+			if (isMinor) return stages[stage].except[code].date;
+			return majorDate;
+		}
+		if (station[0].every(partOpen)) {
+			let note = document.createElement("em");
+			note.append("Operational");
+			output.append(note);
+		} else if ((station[0].length === 1) && (!partOpen(station[0][0]))) {
+			let note = document.createElement("em");
+			note.append("Opening ");
+			note.append(getOpeningDate([station[0][0][0], station[0][0][1]]));
+			output.append(note);
+		} else {
+			for (let [code, stage] of station[0]) {
+				code = code.toLowerCase();
+				let stncodeholder = document.createElement("div");
+				stncodeholder.className = "stn-code-holder";
+				let stncode = document.createElement("div");
+				stncode.className = (code === "rts") ? "stn-code rts" : ("stn-code " + code.slice(0, 2).toLowerCase());
+				stncode.append((code === "rts") ? "RTS" : (code.slice(0, 2).toUpperCase()));
+				
+				if (code !== "rts") { 
+					stncode.append(" ");
+					stncode.append(code.slice(2).toUpperCase());
+				}
+				stncodeholder.append(stncode);
+				let text = document.createElement("em");
+				
+				if (partOpen([code, stage])) {
+					text.append(" operational");
+					} else {
+					text.append(" opening ");
+					text.append(getOpeningDate([code, stage]));
+					text.append(` (${stage})`);
+				}
+				output.append(stncodeholder);
+				output.append(text);
+				output.append(document.createElement("br"));
+			}
+		}
+		output.append(document.createElement("br"));
+		if (futureChange) {
+			let stncodeholder = document.createElement("div");
+			stncodeholder.className = "stn-code-holder";
+			for (code of newcodes) {
+				code = code.toLowerCase();
+				let stncode = document.createElement("div");
+				stncode.className = (code === "rts") ? "stn-code rts" : ("stn-code " + code.slice(0, 2).toLowerCase());
+				stncode.append((code === "rts") ? "RTS" : (code.slice(0, 2).toUpperCase()));
+				stncode.append(" ");
+				if (code !== "rts") stncode.append(code.slice(2));
+				stncodeholder.append(stncode);
+			}
+			let notetext = document.createElement("em");
+			notetext.append("Future ");
+			notetext.append(stncodeholder);
+			notetext.append(" with ");
+			let newerStage = station[0].find(i => i[2])[3];
+			notetext.append(newerStage);
+			notetext.append(" opening ");
+			let newerStageDate = stages[newerStage].date;
+			notetext.append(newerStageDate);
+			output.append(notetext);
+			output.append(document.createElement("br"));
+		}
+		let link = document.createElement("a");
+		link.href = toLTG(stationName);
+		link.target = "_blank";
+		link.append("Read more on Land Transport Guru");
+		output.append(link);
+	}
+
+	const btn = document.querySelector("#go1");
+	btn.addEventListener("click", (e) => {stnCodeSearch(); e.currentTarget.value = "";});
+	const input = document.querySelector("#stncode");
+	input.addEventListener("change", function(e) {
+		e.currentTarget.blur();
+		stnCodeSearch();
+		e.currentTarget.value="";
+	});
+}
+
+{
+	const stnNameSearch = function() {
+		const output = document.querySelector("#stn_output");
+		output.innerHTML = "";
+		let nameInput = document.querySelector("#stnname").value;
+		let formatted = formatname(nameInput);
+		let station = getStationName(formatted);
+		if (!station) {
+			output.innerHTML = "<em>Station not found</em>";
+			return;
+		}
+		let stationName = station[1];
+		let stnCode = document.createElement("div");
+		stnCode.className = "stn-code-holder";
+		let codes = [];
+		for ([i] of station[0]) codes.push(i);
+		let newcodes = [];
+		for (arr of station[0]) newcodes.push(arr[2] ? arr[2] : arr[0]);
+		let futureChange = codes.join() !== newcodes.join();
+		for ([i] of station[0]) {
+			i = i.toLowerCase();
+			let code = document.createElement("div");
+			code.className = "stn-code " + ((i === "rts") ? "rts" :  i.slice(0, 2).toLowerCase());
+			code.append(
+				(i === "rts") ? "RTS" : 
+				i.toUpperCase().slice(0, 2) + " " + i.slice(2).toUpperCase()
+			);
+			stnCode.append(code);
+		}
+		output.append(stnCode);
+		output.append(" ");
+		output.append(stationName);
+		output.append(document.createElement("br"));
+		const partOpen = function([code, stage]) {
+			let majorOpen = stages[stage].open;
+			let minorOpen = (stages[stage].except[code]?.open) ?? majorOpen;
+			if (!majorOpen && !minorOpen) return false;
+			if (majorOpen && minorOpen) return true;
+			if (minorOpen) return true;
+			return false;
+		}
+		const getOpeningDate = function([code, stage]) {
+			let majorDate = stages[stage].date;
+			let isMinor = code in stages[stage].except;
+			if (isMinor) return stages[stage].except[code].date;
+			return majorDate;
+		}
+		if (station[0].every(partOpen)) {
+			let note = document.createElement("em");
+			note.append("Operational");
+			output.append(note);
+		} else if ((station[0].length === 1) && (!partOpen(station[0][0]))) {
+			let note = document.createElement("em");
+			note.append("Opening ");
+			note.append(getOpeningDate([station[0][0][0], station[0][0][1]]));
+			output.append(note);
+		} else {
+			for (let [code, stage] of station[0]) {
+				code = code.toLowerCase();
+				let stncodeholder = document.createElement("div");
+				stncodeholder.className = "stn-code-holder";
+				let stncode = document.createElement("div");
+				stncode.className = (code === "rts") ? "stn-code rts" : ("stn-code " + code.slice(0, 2).toLowerCase());
+				stncode.append((code === "rts") ? "RTS" : (code.slice(0, 2).toUpperCase()));
+
+				if (code !== "rts") { 
+					stncode.append(" ");
+					stncode.append(code.slice(2).toUpperCase());
+				}
+				stncodeholder.append(stncode);
+				let text = document.createElement("em");
+
+				if (partOpen([code, stage])) {
+					text.append(" operational");
+					} else {
+					text.append(" opening ");
+					text.append(getOpeningDate([code, stage]));
+					text.append(` (${stage})`);
+				}
+				output.append(stncodeholder);
+				output.append(text);
+				output.append(document.createElement("br"));
+			}
+		}
+		output.append(document.createElement("br"));
+		if (futureChange) {
+			let stncodeholder = document.createElement("div");
+			stncodeholder.className = "stn-code-holder";
+			for (code of newcodes) {
+				code = code.toLowerCase();
+				let stncode = document.createElement("div");
+				stncode.className = (code === "rts") ? "stn-code rts" : ("stn-code " + code.slice(0, 2).toLowerCase());
+				stncode.append((code === "rts") ? "RTS" : (code.slice(0, 2).toUpperCase()));
+				stncode.append(" ");
+				if (code !== "rts") stncode.append(code.slice(2));
+				stncodeholder.append(stncode);
+			}
+			let notetext = document.createElement("em");
+			notetext.append("Future ");
+			notetext.append(stncodeholder);
+			notetext.append(" with ");
+			let newerStage = station[0].find(i => i[2])[3];
+			notetext.append(newerStage);
+			notetext.append(" opening ");
+			let newerStageDate = stages[newerStage].date;
+			notetext.append(newerStageDate);
+			output.append(notetext);
+			output.append(document.createElement("br"));
+		}
+		let link = document.createElement("a");
+		link.href = toLTG(stationName);
+		link.target = "_blank";
+		link.append("Read more on Land Transport Guru");
+		output.append(link);
+	}
+
+	const btn = document.querySelector("#go2");
+	btn.addEventListener("click", (e) => {stnNameSearch(); e.currentTarget.value="";});
+	const input = document.querySelector("#stnname");
+	input.addEventListener("change", function(e) {
+		e.currentTarget.blur();
+		stnNameSearch();
+		e.currentTarget.value = "";
+	});
+}
